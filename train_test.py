e59a349fc65612d093d3e4b70e6fc2a7658b00d2
nilaysureka <139799789+nilaysureka@users.noreply.github.com>
2024-02-09 19:01:24 +0530

    Update train_test.py

--- train_test.py
+++ train_test.py
@@ -1,6 +1,8 @@
 import cv2 as cv
 import numpy as np
 import matplotlib.pyplot as plt
-img = cv.imread("unnamed.jpg")
-cv.imshow("Display window", img)
-k = cv.waitKey(0)
+print("Imports Complete")
+img =cv.imread("test-img\test.jpg")
+cv.imshow("Output", img)
+cv.waitKey(0)
+cv.destroyAllWindows()
