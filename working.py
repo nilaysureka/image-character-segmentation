6442ca385f9d0e5dd7cc9f2b22b4facffdce1aa2
nilaysureka <139799789+nilaysureka@users.noreply.github.com>
2024-02-09 21:36:19 +0530

    Update working.py

--- working.py
+++ working.py
@@ -6,5 +6,11 @@ imgGray=cv2.cvtColor(img,cv2.COLOR_BGR2GRAY) #Setting Colour tone to grey
 cv2.imshow("Gray Img",imgGray)
 imgCanny =cv2.Canny(img,100,100)
 cv2.imshow("Canny Image",imgCanny) #canny edges detection
-ker=np.ones((5,5),np.uint8)
-imgDial=cv2.dilate(imgCanny,ker,iterations=1)
+'''ker=np.ones((5,5),np.uint8)
+imgDial=cv2.dilate(imgCanny,ker,iterations=1) '''
+imgBlur=cv2.GuasianBlur(imgGray,(7,7),0)
+cv2.imshow("Blur Image",imgBlur)
+
+path='Resources/1.jpg'
+def getContours(img):
+  contours,hierarchy= cv2.findContours(img,cv2.RETR_EXTERNAL)
